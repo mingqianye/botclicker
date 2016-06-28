2063c4a7748ad7b53f59139c530a4e98176c3b5c
Mingqian Ye <mingqian.ye@33across.com>
2016-06-27 22:29:20 -0700

    work

--- app/services/click_weibo_links_service.rb
+++ app/services/click_weibo_links_service.rb
@@ -6,8 +6,10 @@ class ClickWeiboLinksService
     puts "======= Starting at #{DateTime.now} ========="
       browser = ObtainBrowserService.get
       begin
-        browser.goto('https://passport.weibo.cn/signin/login?entry=mweibo')
-        byebug
+        #browser.goto('https://passport.weibo.cn/signin/login?entry=mweibo')
+        browser.goto('http://98.234.187.151:3000/')
+        puts 'done'
+
       rescue Exception => e
         puts "Oops, something is wrong!"
         puts e.backtrace
--- app/services/obtain_browser_service.rb
+++ app/services/obtain_browser_service.rb
@@ -12,9 +12,16 @@ class ObtainBrowserService
     def try_get_browser
       proxy = ObtainProxyService.get
       puts "trying proxy: #{proxy.ip_port}"
-      profile = Selenium::WebDriver::Firefox::Profile.new
-      profile.proxy = Selenium::WebDriver::Proxy.new :http => proxy.ip_port, :ssl => proxy.ip_port
-      Watir::Browser.new :firefox, :profile => profile
+      service_args = ["--proxy=#{proxy.ip_port}", '--proxy-type=http']
+      capabilities = Selenium::WebDriver::Remote::Capabilities.phantomjs("phantomjs.page.settings.userAgent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36", 'phantomjs.page.customHeaders.Referer' => "http://weibo.com/u/2957260672?topnav=1&wvr=6&topsug=1&is_all=1", "phantomjs.cli.args" => service_args)
+      #byebug
+      driver = Selenium::WebDriver.for :phantomjs, :desired_capabilities => capabilities
+      #switches = ["--proxy=#{proxy.ip_port}"]
+      #browser = ::Watir::Browser.new(driver, args: switches)
+      browser = ::Watir::Browser.new driver
+      #profile = Selenium::WebDriver::Firefox::Profile.new
+      #profile.proxy = Selenium::WebDriver::Proxy.new :http => proxy.ip_port, :ssl => proxy.ip_port
+      #Watir::Browser.new :firefox, :profile => profile
     end
   end
 end
