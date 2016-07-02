05be7a43b5aa08dd4df677b0b863006039cb999a
Mingqian Ye <mingqian.ye@33across.com>
2016-07-01 23:49:11 -0700

    work

--- app/services/obtain_target_links_service.rb
+++ app/services/obtain_target_links_service.rb
@@ -3,6 +3,6 @@ class ObtainTargetLinksService
     response = HTTParty.get(Settings.g_spreadsheet_url)
     puts 'Getting links from google spreadsheet'
     lines = response.body.split("\n").map(&:strip)
-    links = lines.map {|l| l.split("\t").second }.compact
+    links = lines.map {|l| l.split("\t").second }.select{|x| x.present?}
   end
 end
