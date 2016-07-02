5b673eba5d4aff14b98089ec94036a646584796b
Mingqian Ye <mingqian.ye@33across.com>
2016-07-01 23:01:37 -0700

    work

--- app/services/click_weibo_links_service.rb
+++ app/services/click_weibo_links_service.rb
@@ -14,7 +14,7 @@ class ClickWeiboLinksService
     ObtainTargetLinksService.get.each do |link|
       puts "Visiting #{link}"
       browser.goto(link)
-      Watir::Wait.until(120) { browser.title.present? }
+      Watir::Wait.until(180) { browser.title.present? }
       puts "Page title: #{browser.title}"
       browser.screenshot.save "screenshots/#{DateTime.now}.png"
       puts 'done'
