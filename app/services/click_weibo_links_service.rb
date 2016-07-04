4ce68e5de82634a015b94c02a1da5808a293d8a4
Mingqian Ye <mingqian.ye@gmail.com>
2016-07-04 11:15:29 -0700

    work

--- app/services/click_weibo_links_service.rb
+++ app/services/click_weibo_links_service.rb
@@ -12,12 +12,17 @@ class ClickWeiboLinksService
   def run
     browser = ObtainBrowserService.new.try_get_browser
     ObtainTargetLinksService.get.shuffle.each do |link|
-      puts "Visiting #{link}"
-      browser.goto(link)
-      Watir::Wait.until(240) { browser.title.present? }
-      puts "Page title: #{browser.title}"
-      browser.screenshot.save "screenshots/#{DateTime.now}.png"
-      puts 'done'
+      begin
+        puts "Visiting #{link}"
+        browser.goto(link)
+        Watir::Wait.until(240) { browser.title.present? }
+        puts "Page title: #{browser.title}"
+        browser.screenshot.save "screenshots/#{DateTime.now}.png"
+        puts 'done'
+      rescue Exception => ex
+        puts "Error message: #{ex.message}"
+        puts "Backtrace: #{ex.backtrace}"
+      end
     end
     browser.close
   end
